20421b5a671c9626b7823f05407c7b78c158c792
Jose Rogerio Bezerra Barbosa Filho - DATAPREVRN <joserogerio.filho@NTLD279425.GOV>
2019-12-18 17:28:13 -0200

    Changing <a> to <Link>

--- react-seed-project/src/components/top-bar/top-bar.js
+++ react-seed-project/src/components/top-bar/top-bar.js
@@ -1,13 +1,13 @@
 import React from 'react';
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './top-bar.scss';
 
 export default function TopBar() {
   return (
     <div className={"top-bar"}>
-      <a href='/#/' >Home</a>
-      <a href='/#/screen1' >Screen 1</a>
-      <a href='/#/screen2'>Screen 2</a>
+      <Link to='/' >Home</Link>
+      <Link to='/screen1' >Screen 1</Link>
+      <Link to='/screen2'>Screen 2</Link>
     </div>
   )
 }
