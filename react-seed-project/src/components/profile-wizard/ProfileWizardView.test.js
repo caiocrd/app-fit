10f2ab02c7b0eed858dc086d61dcbd45734ba26b
José Rogério Bezerra Barbosa Filho <roger.sama@gmail.com>
2020-02-06 00:47:54 -0300

    Added unit tests for the ProfileWizardView and Screen1 components

---
+++ react-seed-project/src/components/profile-wizard/ProfileWizardView.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+//import ProfileWizardView from './ProfileWizardView'
+
+test('masks the input for weight', () => {
+    let teste = false;
+
+    expect(teste).toBeFalsy();
+})
---
+++ react-seed-project/src/components/screen1/Screen1.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StaticRouter } from 'react-router'
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Screen1 from './Screen1'
+
+let container;
+
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+test('has a "Screen #1" text', () => {
+    act(() => {
+        render(
+            <StaticRouter>
+                <Screen1 />
+            </StaticRouter>
+            ,container
+        );
+    });
+
+    const paragraph = container.getElementsByTagName("p")[0];    
+    expect(paragraph.textContent).toBe("Screen #1");   
+});
