bb18f0b233649d16017341badff00275e86c661b
José Rogério Bezerra Barbosa Filho <roger.sama@gmail.com>
2020-02-09 21:34:52 -0300

    Profile screen markup

--- react-seed-project/src/components/profile/ProfileContainer.js
+++ react-seed-project/src/components/profile/ProfileContainer.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ProfileView from './ProfileView.js';
 
 export default function ProfileContainer(props) {
+    const onStart = () => {
+        props.history.push({ pathname: `/` });
+    }
+
     return(
-        <ProfileView profile={props.location.state.profile} />
+        <ProfileView profile={props.location.state.profile} onStart={onStart} />
     );
 }
--- react-seed-project/src/components/profile/ProfileView.js
+++ react-seed-project/src/components/profile/ProfileView.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-export default function ProfileView(props) {
-    console.log(props.profile);
+export default function ProfileView({profile, onStart}) {
+  console.log(profile);
     
   return (
-    <div>Estou em profile!</div>
+    <div>
+      <div>
+        <Typography variant="h5" component="h2">Perfil Fitness</Typography>
+        <Typography variant="subtitle2">DICAS DE DIETA E EXERCÍCIOS</Typography>
+      </div>
+
+      <div>
+        <Typography variant="h5" component="h3">Dieta</Typography>
+        <div>Img</div>
+        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et tristique dui. Phasellus sit amet lacus dignissim, condimentum ligula vitae, iaculis nibh.</div>
+        <div>Img</div><div>Img</div><div>Img</div>
+      </div>
+
+      <div>
+        <Typography variant="h5" component="h3">Exercícios</Typography>
+        <div>Img</div>
+        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et tristique dui. Phasellus sit amet lacus dignissim, condimentum ligula vitae, iaculis nibh.</div>
+        <div>Img</div><div>Img</div><div>Img</div>
+      </div>
+      
+      <div>
+        <Button variant="outlined">REFAZER</Button>
+        <Button variant="contained" color="primary" onClick={() => onStart()}>COMEÇAR</Button>
+      </div>
+    </div>
   );
 }
